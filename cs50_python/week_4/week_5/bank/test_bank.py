67e9644d3b3bdfd587e5e2247807a1d6350ff52d
Nitesh S <132873098+iBuddyBobby@users.noreply.github.com>
2024-04-22 06:55:53 +0530

    Delete cs50_python/week_4/week_5 directory

--- cs50_python/week_4/week_5/bank/test_bank.py
+++
@@ -1,9 +0,0 @@
-import bank
-
-
-def test_starts_with_hello():
-    assert bank.value("Hello") == 0
-    assert bank.value("How") == 20
-    assert bank.value("Sup") ==100
-    assert bank.value("Good morning") == 100
-
--- cs50_python/week_4/week_5/twttr/test_twttr.py
+++
@@ -1,8 +0,0 @@
-from twttr import shorten
-def test():
-    assert shorten("twitter") == "twttr"
-    assert shorten("TWITTER") == "TWTTR"
-    assert shorten("t3113r") == "t3113r"
-    assert shorten("T@!!*r") == "T@!!*r"
-
-
